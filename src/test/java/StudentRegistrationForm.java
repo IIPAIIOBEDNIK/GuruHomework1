30ffb811c69e7f84a4a2c91c65fceba6eaf2b716
IIPAIIOBEDNIK <pereponov_alexander@mail.ru>
2021-07-08 12:41:22 +0300

    Modifided test and added new assertions

--- src/test/java/StudentRegistrationForm.java
+++ src/test/java/StudentRegistrationForm.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class StudentRegistrationForm {
 
@@ -21,22 +23,30 @@ public class StudentRegistrationForm {
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("Pereponov");
         $("#userEmail").setValue("pereponov_alexander@mail.ru");
-        $("label[for='gender-radio-1']").click();
-        $("#userNumber").setValue("89169163660");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("9169163660");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOptionContainingText("1998");
         $(".react-datepicker__month-select").selectOptionContainingText("March");
-        $x("//div[@class='react-datepicker__week']/*[text()=27]").click();
+        $(".react-datepicker__day--027").click();
         $("#subjectsInput").setValue("Maths").pressEnter();
-        $("label[for='hobbies-checkbox-3']").click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
         $("#uploadPicture").uploadFile(new File("src/test/resources/road.jpg"));
         $("#currentAddress").setValue("Russia");
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Noida").pressEnter();
-        $("#submit").click();
-        //Asserts
+        $("#submit").scrollTo().click();
+        //Assertions
         $(".table-responsive").shouldHave(
                 text("Alex Pereponov"),
+                text("pereponov_alexander@mail.ru"),
+                text("Male"),
+                text("9169163660"),
+                text("27 March,1998"),
+                text("Maths"),
+                text("Music"),
+                text("road.jpg"),
+                text("Russia"),
                 text("NCR Noida"));
     }
 
